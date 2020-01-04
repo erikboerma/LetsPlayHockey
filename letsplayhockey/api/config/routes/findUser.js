f444df3394ad91935aa992c60552e563194b7492
Ravi Jethva <rrjethva@gmail.com>
2020-01-04 13:10:06 -0500

    jwt auth added

---
+++ letsplayhockey/api/config/routes/findUser.js
@@ -0,0 +1,26 @@
+import passport from 'passport';
+
+module.exports = app => {
+    app.get('/findUser', (req, res, next) => {
+        passport.authenticate('jwt', { session: false }, (err, user, info) => {
+            if (err) {
+                console.log(err);
+            }
+            if (info != undefined) {
+                console.log(info.message);
+                res.send(info.message);
+            } else {
+                console.log('user found in db from route');
+                res.status(200).send({
+                    auth: true,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    email: user.email,
+                    username: user.username,
+                    password: user.password,
+                    message: 'user found in db',
+                });
+            }
+        })(req, res, next);
+    });
+};
---
+++ letsplayhockey/api/models/user.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, type) => sequelize.define('user', {
+    id: {
+        type: type.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    first_name: type.STRING,
+    last_name: type.STRING,
+    email: {
+        type: type.STRING,
+        allowNull: false,
+    },
+    username: {
+        type: type.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: type.STRING,
+        allowNull: false,
+    },
+    resetPasswordToken: type.STRING,
+    resetPasswordExpires: type.DATE,
+});
