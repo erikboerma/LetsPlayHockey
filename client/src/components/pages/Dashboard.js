fdc9c51a94f2cf180f354a1c148b5f0322d1baf0
ErikBoerma <eboerma@gmail.com>
2020-01-13 22:04:31 -0500

    added template for dashboard, linking issue

---
+++ client/src/components/pages/Dashboard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './style.css'
+
+function Dashboard() {
+    return (
+        <div class="container">
+            <div class="row">
+                <div class="column">
+                    <h1>PLayer profile</h1>
+                    <h3>Edit my profile</h3>
+                    
+                </div>
+                <div class="column">
+                    <h1>Available games</h1>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
--- server.js
+++ server.js
@@ -25,6 +25,7 @@ require("./routes/register")(app);
 require("./routes/registerUser")(app);
 require("./routes/loginUser")(app);
 
+
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
 
 module.exports = app;
