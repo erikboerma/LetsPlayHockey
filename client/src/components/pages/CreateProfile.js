f39ec1ff5599b45f440cd9f2a254c3c0f553092c
john Meng <john.meng014@gmail.com>
2020-01-14 19:34:56 -0500

    updated with dashboard

--- client/src/components/pages/CreateProfile.js
+++ client/src/components/pages/CreateProfile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CreateProfile from '../forms/CreateProfile/CreateProfile'
-import rinkLine from "../../assets/images/icerink.png"
+
 
 function Create() {
     return (
