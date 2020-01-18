997c95ed7b13f27bb932c105e74ffbc3664fbba2
john Meng <john.meng014@gmail.com>
2020-01-18 11:39:32 -0500

    added stuff

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,4 +1,4 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 // import { Button, Modal } from 'react-bootstrap';
 
 // function showModal() {
@@ -8,7 +8,35 @@
 //     const handleShow = () => setShow(true);
 
 
+
+
 //     return (
+
+<div class="modal fade" id="centralModalSm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+
+  <!-- Change class .modal-sm to change the size of the modal -->
+  <div class="modal-dialog modal-sm" role="document">
+
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title w-100" id="myModalLabel">Modal title</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary btn-sm">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 //         <>
 //             <Button variant="primary" onClick={handleShow}>
 //                 Launch demo modal
@@ -32,4 +60,4 @@
 //     )
 // };
 
-// export default showModal;
+export default showModal;
