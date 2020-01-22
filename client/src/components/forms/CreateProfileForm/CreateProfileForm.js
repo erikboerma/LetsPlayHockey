36c8df7c948a010c2d8d91b17fac2cbb6b24c2a7
Valon Rama <vrama21@gmail.com>
2020-01-22 01:30:44 -0500

    Refactored register container and many of its underlying components

--- client/src/components/forms/CreateProfileForm/CreateProfileForm.js
+++ client/src/components/forms/CreateProfileForm/CreateProfileForm.js
@@ -7,24 +7,17 @@ import {
   MDBCardBody,
   MDBBtn
 } from "mdbreact";
-import Checkbox from "../../Checkbox/Checkbox";
 import Select from "react-select";
-import { position, shot, skillLevel, days, notice } from "../../../constants";
+import { shot, skillLevel, notice } from "../../../constants";
 import "./createProfileForm.css";
-
-const mapArray = (arr, state) => {
-  let newArr = [];
-  arr.map((key, i) => {
-    newArr.push({
-      state: state,
-      label: key,
-      value: i
-    });
-  });
-  return newArr;
-};
+import mapSelectArray from '../../../utils';
 
 const CreateProfileForm = props => {
+
+  const handleChange = selectedOption => {
+    props.setUser({selectedOption}.value)
+  }
+
   if (props.currentStep !== 2) {
     return null
   }
@@ -50,8 +43,13 @@ const CreateProfileForm = props => {
                 <div className="position-select">
                   <Select
                     placeholder="Shot"
-                    options={mapArray(shot, "shot")}
-                    onChange={props.handleChange}
+                    options={mapSelectArray(shot)}
+                    onChange={handleChange}
+                    // onChange={props.setUser({ ...props.user, selectedOption.value})}
+                    // onChange={e =>
+                    //   console.log(e.target.value)
+                    //   // props.setUser({ ...props.user, shot: e.target.value })
+                    // }
                   />
                 </div>
               </div>
@@ -64,8 +62,10 @@ const CreateProfileForm = props => {
                 <div className="position-select">
                   <Select
                     placeholder="Skill Level"
-                    options={mapArray(skillLevel, "skillLevel")}
-                    onChange={props.handleChange}
+                    options={mapSelectArray(skillLevel, "skillLevel")}
+                    onChange={e =>
+                      props.setUser({ ...props.user, skillLevel: e.target.value.value })
+                    }
                   />
                 </div>
               </div>
@@ -78,15 +78,16 @@ const CreateProfileForm = props => {
                 <div className="position-select">
                   <Select
                     placeholder="Notice Needed"
-                    options={mapArray(notice, "notice")}
-                    onChange={props.handleChange}
+                    options={mapSelectArray(notice, "notice")}
+                    onChange={e =>
+                      props.setUser({ ...props.user, notice: e.target.value.value })
+                    }
                   />
                 </div>
               </div>
               <hr />
 
               <div className="row">
-
                 <div className="text-left mb-3">
                   <MDBBtn
                     type="button"
